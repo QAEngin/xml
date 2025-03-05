d2c63800e4b09ef716a1c7a5f8a220371fe9cc8d
QAEngin <87521993+QAEngin@users.noreply.github.com>
2025-03-05 13:46:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-function downloadXML() {
+function openURL() {
     const url = 'https://raw.githubusercontent.com/QAEngin/xml/main/oryeuda.html';
-
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch file: ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then(xmlText => {
-            const blob = new Blob([xmlText], { type: 'application/html' });
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'oryeuda.html';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        })
-        .catch(error => {
-            alert('Failed to download file: ' + error.message);
-            console.error(error);
-        });
 }
 
 function copyToClipboard() {
